768cd644ae6a7886b53cd8b62f3117c2ff8dc10f
Juri Burdugov <Juri@VTE-Teichmann.local>
2020-04-24 16:01:14 +0200

    Projektdateien hinzufügen.

---
+++ SplashTest/App.xaml.cs
@@ -0,0 +1,49 @@
+﻿using MahApps.Metro.Controls;
+using MahApps.Metro.Controls.Dialogs;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace SplashTest
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+
+        protected async override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            MetroWindow splash = new SplashScreenMetro("Data Loading in Progress.....");
+
+            splash.Show();
+            
+            var controller = await splash.ShowProgressAsync("Tets","Test", false, new MetroDialogSettings() { MaximumBodyHeight = 70, DialogMessageFontSize = 18  }) as ProgressDialogController;
+
+            controller.SetIndeterminate();
+
+            MainWindow main = new MainWindow();
+
+            main.InitializeComponent();
+
+            Current.MainWindow = main;
+
+            await Task.Delay(2500);
+
+            await controller.CloseAsync();
+
+            splash.Close();
+
+            await Task.Delay(1000);
+
+            main.Show();
+        }
+    }
+}
---
+++ SplashTest/SplashScreenMetro.xaml.cs
@@ -0,0 +1,47 @@
+﻿using MahApps.Metro.Controls;
+using MahApps.Metro.Controls.Dialogs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace SplashTest
+{
+    /// <summary>
+    /// Interaktionslogik für SplashScreenMetro.xaml
+    /// </summary>
+    public partial class SplashScreenMetro : MetroWindow
+    {
+        public string Message
+        {
+            get { return (string)GetValue(MessageProperty); }
+
+            set { SetValue(MessageProperty, value); }
+        }
+
+        public static readonly DependencyProperty MessageProperty = DependencyProperty.Register("Message", typeof(string), typeof(Label), new UIPropertyMetadata("Working, wait...."));
+
+        public SplashScreenMetro()
+        {
+            InitializeComponent();
+        }
+
+        public SplashScreenMetro(string message)
+        {
+            InitializeComponent();
+
+            this.Message = message;
+
+            lblmessage.Content = this.Message;
+        }
+    }
+}
